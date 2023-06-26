8c400070f1787ff4aeaac345d510e5fef3553c4f
tanaka <nitnc.tanaka.shikkobu@nezumi.be>
2023-06-27 01:31:40 +0900

    add Index

--- Sources/App/Migrations/SendEvent+IndexMigration.swift
+++ Sources/App/Migrations/SendEvent+IndexMigration.swift
@@ -6,11 +6,15 @@ extension SendEvent {
         
         func prepare(on database: Database) async throws {
             let sql = database as! SQLDatabase
+            _ = try await sql.raw("CREATE INDEX send_events_from_username_index ON send_events (from_username)").all();
+            _ = try await sql.raw("CREATE INDEX send_events_to_username_index ON send_events (to_username)").all();
             _ = try await sql.raw("CREATE INDEX send_events_created_at_index ON send_events (created_at)").all();
         }
         
         func revert(on database: Database) async throws {
             let sql = database as! SQLDatabase
+            _ = try await sql.raw("DROP INDEX send_events_from_username_index").all();
+            _ = try await sql.raw("DROP INDEX send_events_to_username_index").all();
             _ = try await sql.raw("DROP INDEX send_events_created_at_index").all();
         }
     }
